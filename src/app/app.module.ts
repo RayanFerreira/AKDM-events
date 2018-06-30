551264cd1f1f266fa8979523285dfb53e72ef034
RayanFerreira <rayan.raf97@gmail.com>
2018-06-30 17:30:40 -0300

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,30 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
+
+import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/home/home';
+
+import { IntroPageModule } from '../pages/intro/intro.module';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IntroPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
